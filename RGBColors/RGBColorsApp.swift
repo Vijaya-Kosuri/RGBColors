f22042a756c978f30ef3f26d6134ae73991750c0
Vijaya Kosuri <vij.kosuri@gmail.com>
2023-09-03 18:30:31 -0400

    Initial Commit

---
+++ RGBColors/RGBColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBColorsApp.swift
+//  RGBColors
+//
+//  Created by Srinivas Pala on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RGBColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
